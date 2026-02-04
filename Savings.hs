c925866d4bc2c8b990649b9022e61be70e0879e9
Mokut001 <mokutoton01@gmail.com>
2026-02-04 21:48:00 +0100

    Add files via upload

---
+++ Savings.hs
@@ -0,0 +1,22 @@
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Savings where
+
+import PlutusTx
+import PlutusTx.Prelude
+
+data SavingsDatum = SavingsDatum
+  { target :: Integer
+  , saved  :: Integer
+  }
+
+{-# INLINABLE validateSave #-}
+validateSave :: SavingsDatum -> Integer -> Bool
+validateSave datum amount =
+  traceIfFalse "Target reached" (saved datum < target datum) &&
+  traceIfFalse "Invalid amount" (amount > 0)
+
+PlutusTx.compile [|| validateSave ||]
